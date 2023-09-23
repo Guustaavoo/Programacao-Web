228ac3f9706bdb7a1f85db735ede65d8a85c98e1
Gustavo <gustavohsr11@gmail.com>
2023-09-22 22:07:13 -0300

    Lista JS Q5

--- LISTA JS/app.js
+++ LISTA JS/app.js
@@ -2,8 +2,10 @@ var soma = 10 + 20;
 var diferenca = 30 - 10;
 var multiplicacao = 15 * 2;
 var divisao = 100 / 5;
+var restoDivisao = 45 % 6;
 
 console.log("Soma:", soma);
 console.log("Diferença:", diferenca);
 console.log("Multiplicação: ", multiplicacao);
 console.log("Divisão: ", divisao);
+console.log("Resto da divisão: ", restoDivisao);
