ec49cacd0566347162cceceb33ca161b5bd35e82
Gustavo <gustavohsr11@gmail.com>
2023-09-27 23:42:19 -0300

    Lista JS Q7

--- LISTA JS/app.js
+++ LISTA JS/app.js
@@ -9,6 +9,9 @@ for (var i = 0; i < numeros.length; i++) {
     somanumeros += numeros[i];
 }
 var media = somanumeros / numeros.length;
+var valor = 80;
+var porcentagem = 25;
+var resultado = (porcentagem / 100) * valor;
 
 console.log("Soma:", soma);
 console.log("Diferença:", diferenca);
@@ -16,3 +19,4 @@ console.log("Multiplicação: ", multiplicacao);
 console.log("Divisão: ", divisao);
 console.log("Resto da divisão: ", restoDivisao);
 console.log("Números:", numeros, "Soma dos números:", somanumeros, "Média dos números:", media);
+console.log("25% de 80 é igual a:", resultado);
